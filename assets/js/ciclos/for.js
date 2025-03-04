2cd20d16dae848aa23167c90be699f81846f0af4
adrielisa <danielganer67@gmail.com>
2025-03-03 23:43:10 -0500

    FOR OF
    Se agregó la documentación de For of

--- assets/js/ciclos/for.js
+++ assets/js/ciclos/for.js
@@ -13,12 +13,20 @@ for( let i = 0; i < heroes.length; i++ ) {
 }
 
 /*Bucle for..in, se usa para íterar sobre las propiedades enumerables de un objeto o los índices de un arreglo
-b */
+let i in heroes: i toma el valor de cada índice del arreglo heroes
+consolo.log(heroes[i]): Imprime el valor de la posición i del arreglo heroes
+FOR IN ITERA SOBRE LOS ÍNDICES DEL ARREGLO
+ */
 console.warn('For in');
 for( let i in heroes ) {
     console.log( heroes[i] );
 }
 
+/*For..of, se usa para iterar sobre los valores de un objeto iterable, como lo es un arreglo  
+let heroe of heroes: heroe toma el valor de cada elemento del arreglo heroes
+console.log(heroe): Imprime el valor de heroe
+FOR OF ITERA SOBRE LOS VALORES DEL ARREGLO
+*/
 console.warn('For of');
 for( let heroe of heroes ){
     console.log( heroe );
