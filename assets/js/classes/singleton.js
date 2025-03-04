91fd2d54c96ae628780fb95d113465beec3435d8
adrielisa <danielganer67@gmail.com>
2025-03-04 14:21:31 -0500

    Singleton
    Documentación de sigleton

--- assets/js/classes/singleton.js
+++ assets/js/classes/singleton.js
@@ -1,4 +1,17 @@
-//Un singleton es una instancia única de la clasem 
+/*Un singleton es un patrón de diseño que asegura que ina clase tenga solo una instancia y proporciona 
+un punto de acceso global a esa instancia
+
+En este código:
+
+La clase Singleton tiene una propiedad estática instancia que almacena la única instancia de la clase.
+
+El constructor verifica si ya existe una instancia. Si existe, retorna esa instancia en lugar de crear una nueva.
+Cuando se crean múltiples instancias de Singleton, todas ellas referencian la misma instancia, asegurando que 
+solo haya una instancia de la clase en todo el programa.
+
+Este patrón es útil cuando necesitas asegurarte de que solo haya una instancia de una clase en todo el sistema, 
+como en el caso de una conexión a una base de datos o un manejador de configuración.
+*/
 
 class Singleton {
 
