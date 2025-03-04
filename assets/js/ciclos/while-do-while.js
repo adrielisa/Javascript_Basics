d2eb25d87a68fcf28690f984e24b2dad0d691753
adrielisa <danielganer67@gmail.com>
2025-03-03 23:50:28 -0500

    while-do-while
    Se añadió la documentación de while-do-while

--- assets/js/ciclos/while-do-while.js
+++ assets/js/ciclos/while-do-while.js
@@ -1,15 +1,17 @@
 
-
+//Arreglo carros con una lista de marcas
 const carros = ['Ford','Mazda','Honda','Toyota'];
 
-let i = 0;
+let i = 0; 
 
+//Arreglo que itera sobre carros, e imprime cada elemento hasta que i sea igual a la longitud del arreglo
 // while( i < carros.length ) {
 //     console.log( carros[i] );
-//     // i = i + 1;
 //     i++;
 // }
 
+
+//El bucle while continua mientras carros [i] sea verdadero, es decir, mientras haya un elemento en la posición i del arreglo carros
 console.warn('While')
 // undefined
 // null
@@ -33,4 +35,15 @@ do {
     console.log( carros[j]);
     j++;
 } while( carros[j] );
+
+/* 
+Bucle while:
+
+Itera sobre el arreglo carros mientras haya elementos en la posición i.
+Usa continue para saltar una iteración específica (cuando i es 1).
+
+Bucle do...while:
+Intenta iterar sobre el arreglo carros comenzando desde un índice 10, lo que resulta 
+en una única iteración porque carros[10] es undefined.
+*/
 
